3b29783c3fa088d3240c248c8e312931cd3f66ae
andrej <andersontwentytwo@gmail.com>
2023-06-14 22:51:58 +0300

    transferd part of file service onto the api service

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,14 @@ import { userRouter } from './routes/userRouter';
 import { fileRouter } from './routes/fileRouter';
 import captchaController from './controllers/captchaController';
 import path from 'path';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 function main() {
   const app = express();
   const publicPath = path.join(__dirname, '..', 'public');
+  const PORT = process.env.PORT;
 
   app.use(cors());
   app.use(express.json());
@@ -19,8 +23,8 @@ function main() {
 
   app.post('/verify-token', captchaController.verifyToken);
 
-  app.listen(process.env.PORT, () => {
-    global.console.log(`Server listening on port ${process.env.PORT}`);
+  app.listen(PORT, () => {
+    global.console.log(`Server listening on port ${PORT}`);
   });
 }
 
--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,10 +1,6 @@
 import 'path';
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import { CreateFileClientPayload } from '../types/commentTypes';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 class FileService {
   async getFile(fileName: string) {
@@ -44,46 +40,6 @@ class FileService {
     } catch (error) {
       console.log(error);
       throw error;
-    }
-  }
-
-  async createFile(file: CreateFileClientPayload, commentId: number) {
-    const { fileName, dataUrl } = file;
-
-    const uniqueFileName = `${Date.now()}-${fileName}`;
-    const safeFileName = uniqueFileName
-      .replace(/[^a-z0-9.]/gi, '_')
-      .toLowerCase();
-    const filePath = path.resolve(
-      __dirname,
-      '..',
-      '..',
-      'public',
-      safeFileName
-    );
-    const mimeType = dataUrl.split(';')[0].split(':')[1];
-    const base64String = dataUrl.split(',')[1];
-    const binaryData = Buffer.from(base64String, 'base64');
-
-    try {
-      const fileData = await Promise.all([
-        await prisma.file.create({
-          data: {
-            name: safeFileName,
-            commentId,
-            size: binaryData.length,
-            type: mimeType,
-            url: `/${safeFileName}`,
-          },
-        }),
-        await fs.writeFile(filePath, binaryData),
-      ]);
-
-      console.log('create File', fileData[0]);
-
-      return fileData[0];
-    } catch (error) {
-      console.log(error);
     }
   }
 }
