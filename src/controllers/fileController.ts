9a8c116f1849ea1231426d97756da13d20250883
andrej <andersontwentytwo@gmail.com>
2023-06-16 00:56:10 +0300

    downloading files from AWS

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -1,19 +1,42 @@
 import fileService from '../services/fileService';
 import { Request, Response } from 'express';
 import path from 'path';
+import { GetObjectCommand, S3 } from '@aws-sdk/client-s3';
+
+const s3Client = new S3({});
 
 class FileController {
   async getFile(req: Request, res: Response) {
     console.log(req.params);
 
     try {
-      const dataUrl = await fileService.getFile(req.params.filename);
+      const getObjectParams = {
+        Bucket: 'dzen-bucket',
+        Key: req.params.filename,
+      };
 
-      res.status(200).send(dataUrl);
-    } catch (err) {
-      console.log(err);
-      res.sendStatus(404);
+      const response = await s3Client.send(new GetObjectCommand(getObjectParams));
+      const responseString = await response.Body?.transformToString('base64');
+      // ArrayBuffer
+      // const blob = new Blob([arrayBuffer], {type: response.ContentType});
+
+
+      const dataUrl = `data:${response.ContentType};base64,${responseString}`;
+      res.status(200).json(dataUrl);
+      console.log('Successfully generated dataUrl');
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(400);
     }
+
+    // try {
+    //   const dataUrl = await fileService.getFile(req.params.filename);
+    //
+    //   res.status(200).send(dataUrl);
+    // } catch (err) {
+    //   console.log(err);
+    //   res.sendStatus(404);
+    // }
   }
 
   async createFile(req: Request, res: Response) {
